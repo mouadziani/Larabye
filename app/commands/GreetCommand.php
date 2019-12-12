4cf78e83c86f415ce072a68b3f55f9c9330d1e4f
MouadBH <mouad123b@gmail.com>
2019-12-12 17:05:28 +0100

    Rock CLI infrastructer

---
+++ app/commands/GreetCommand.php
@@ -0,0 +1,54 @@
+<?php
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GreetCommand extends Command
+{
+    protected $commandName = 'greet';
+    protected $commandDescription = "Greets Someone";
+
+    protected $commandArgumentName = "name";
+    protected $commandArgumentDescription = "Who do you want to greet?";
+
+    protected $commandOptionName = "cap"; // should be specified like "app:greet John --cap"
+    protected $commandOptionDescription = 'If set, it will greet in uppercase letters';    
+
+    protected function configure()
+    {
+        $this
+            ->setName($this->commandName)
+            ->setDescription($this->commandDescription)
+            ->addArgument(
+                $this->commandArgumentName,
+                InputArgument::OPTIONAL,
+                $this->commandArgumentDescription
+            )
+            ->addOption(
+               $this->commandOptionName,
+               null,
+               InputOption::VALUE_NONE,
+               $this->commandOptionDescription
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument($this->commandArgumentName);
+
+        if ($name) {
+            $text = 'Hello '.$name;
+        } else {
+            $text = 'Hello';
+        }
+
+        if ($input->getOption($this->commandOptionName)) {
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+    }
+}
