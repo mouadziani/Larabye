1626e39d8f022c015a6b43de972cc5fb96a6247c
MouadBH <mouad123b@gmail.com>
2019-12-13 15:38:37 +0100

    using 'namespace'

--- src/commands/ClearcacheCommand.php
+++ src/commands/ClearcacheCommand.php
@@ -1,4 +1,6 @@
 <?php
+namespace Console\Commands;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
